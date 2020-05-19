af896b0e49297a18537e5d4e85fa2e9fc2fd0c0e
scrabill <shannon.crabill@gmail.com>
2020-05-19 18:54:31 -0400

    Add footer and link styling

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ function App() {
       <h1>Bubble Wrap</h1>
       <p>Click on a bubble to pop it</p>
       <BubbleContainer />
+
+      <footer>
+        <p><a href="https://github.com/scrabill/bubble-wrap" target="_blank">Bubble Wrap</a> By Shannon Crabill as part of <a href="https://shannoncrabill.com/blog/7-days-7-websites/" target="_blank">#7Days7Websites</a>.</p>
+      </footer>
     </div>
   );
 }
