bc3358587f428e2d7154f7213926af5704745c48
scrabill <shannon.crabill@gmail.com>
2020-05-19 14:35:13 -0400

    Adds title and instructions

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Bubble from './bubble.js'
+import BubbleContainer from './bubbleContainer.js'
 
 function App() {
   return (
     <div className="App">
-      <Bubble />
+      <h1>Bubble Wrap</h1>
+      <p>Click on a bubble to pop it</p>
+      <BubbleContainer />
     </div>
   );
 }
